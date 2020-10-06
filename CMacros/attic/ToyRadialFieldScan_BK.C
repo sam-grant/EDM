2c2701d0cda27afe1eefc74c8c57d52c3c719306
Samuel Grant <samuelgrant@x.local>
2020-10-05 21:05:02 -0500

    Restructed toy scan to draw a random number for the y-position, and backed up old scan

--- CMacros/attic/ToyRadialFieldScan_BK.C
+++ CMacros/attic/ToyRadialFieldScan_BK.C
@@ -21,7 +21,7 @@
 #include "TRandom3.h"
 
 // ==================== Initialise global parameters ====================
-const int N_QHV = 2;
+const int N_QUAD = 2;
 const int N_FIELD = 4;
 const double BR_RES = 8; // ppm, the size of the field that would kill our measurement
 const int N_SUBRUNS = 8;
@@ -30,40 +30,46 @@ const int SUBRUNS[N_SUBRUNS] = {25, 50, 75, 100, 125, 150, 175, 200};
 const double SIGMA[N_SUBRUNS] = {0.0456595, 0.0247454, 0.018954, 0.015932, 0.0148505, 0.0132568, 0.0120878, 0.0111837}; 
 const double CTAGS[N_SUBRUNS] = {401058, 1.36725e+06, 2.32944e+06, 3.29551e+06, 3.79245e+06, 4.75873e+06, 5.72386e+06, 6.68638e+06};
 
-const double QHV[N_QHV] = {16, 20}; // Two quad settings, kV
+const double QHV[N_QUAD] = {16, 20}; // Two quad settings, kV
 const double BR_APP[N_FIELD] = {-30, -10, 10, 30}; // Applied radial field, ppm
 
-// ==================== HELPER FUNCTIONS ====================
 
-// This is an approximation! 
+// Just put this in a function so it can be easily expanded
 double yPosition(double Br_true, double QHV) {
-
 	return Br_true / QHV;
-
 }
 
-double RadialFieldPrecision(TF1 *fit) { 
-
-	double c = fit->GetParameter(0);
-	double cerr = fit->GetParError(0);
-	double m = fit->GetParameter(1);
-	double merr = fit->GetParError(1);
-	double Br =  - c / m;
+double BrResError(double Br, double c, double cerr, double m, double merr) {
 
 	return Br * sqrt( pow(cerr/c, 2) + pow(merr/m, 2) );
-
 }
 
-// Get a random value for the y-poistion based on Gaussian smearing
-double GausSmearing(double yPos, int i_subrun) {
+// Apply guass
+// From CaloMeanPos.C 
+
+double GausSmearing(double yPos, int i_field, int i_quad, int i_subrun) {
 
 	TRandom3 rndm;
-	return rndm.Gaus(yPos,SIGMA[i_subrun]);
 
+	TH1D *hist= new TH1D("Gaus","Gaus",1000,yPos-0.1,yPos+0.1);
+	//TH1D *hist; // 
+	//if(yPos >= 0) hist = new TH1D("a","a",1000,yPos-(yPos*0.5),yPos+(yPos*0.5));
+	//else hist = new TH1D("a","a",1000,yPos+(yPos*0.5),yPos-(yPos*0.5));
+
+	for(int i = 0; i<CTAGS[i_subrun]; i++) {
+
+		hist->Fill(rndm.Gaus(yPos,SIGMA[i_subrun]));
+
+	}
+
+	DrawTH1(hist,("Sub-runs "+std::to_string(int(SUBRUNS[i_subrun]))+", field setting "+std::to_string(int(BR_APP[i_field]))+" ppm, quad setting "+std::to_string(int(QHV[i_quad]))+" kV").c_str(),("../Images/MC/ToyRadialFieldScan/SmearingHists/hist_"+std::to_string(i_field)+"_"+std::to_string(i_quad)+"_"+std::to_string(i_subrun)).c_str());
+
+	double result = hist->GetRMS();
+	delete hist;
+	return result;
 }
 
-
-// ==================== CUSTOM PLOTTING ====================
+// ==================== Custom plotting ====================
 
 void DrawQuadScanFits(std::vector<TGraphErrors*> graphs, std::string title, std::string fname, double ymin, double ymax, std::string func ) {
 
@@ -78,12 +84,12 @@ void DrawQuadScanFits(std::vector<TGraphErrors*> graphs, std::string title, std:
 	graphs.at(0)->GetXaxis()->CenterTitle(true);
 	graphs.at(0)->GetYaxis()->CenterTitle(true);
 	graphs.at(0)->GetYaxis()->SetMaxDigits(4);
+	//graphs.at(0)->SetMarkerStyle(20); //  Full circle
 	graphs.at(0)->GetYaxis()->SetRangeUser(ymin,ymax);
 
 	TLegend *l = new TLegend(0.81,0.35,0.99,0.65);
-
+	//l->SetHeader("Applied B_{r}");
 	l->SetBorderSize(0);
-
 	// Load legend entries backwards
 	for( int i = graphs.size()-1; i>-1; i--) {
 		l->AddEntry(graphs.at(i), FormatNegativeNumber(BR_APP[i])+" ppm");
@@ -96,16 +102,14 @@ void DrawQuadScanFits(std::vector<TGraphErrors*> graphs, std::string title, std:
 		fit->SetLineColor(i+1); 
 		graphs.at(i)->SetMarkerColor(i+1);
 		graphs.at(i)->SetLineColor(i+1);
-
 		if(i==0) graphs.at(i)->Draw("AP");
 		else {
+
 			graphs.at(i)->Draw("P SAME");
+			
 		}
-
 		fit->Draw("same");
-
 	}
-
 	l->Draw("same");
 
 	c->SaveAs((fname+".pdf").c_str());
@@ -118,7 +122,7 @@ void DrawQuadScanFits(std::vector<TGraphErrors*> graphs, std::string title, std:
 
 }
 
-void DrawRadialFieldLineFit(TGraphErrors *graph, std::string title, std::string fname, std::string func) {
+void DrawRadialFieldLineFit(TGraphErrors *graph, std::string title, std::string fname) {
 
 	TCanvas *c = new TCanvas("c","c",800,600);
 
@@ -126,21 +130,22 @@ void DrawRadialFieldLineFit(TGraphErrors *graph, std::string title, std::string 
 	gPad->Update();
 
 	// Get function
-	TF1 *fit = graph->GetFunction(func.c_str());
-	fit->SetLineWidth(2);
-	fit->SetLineColor(kRed);
+	TF1 *func = graph->GetFunction("lineFit2");
 
-	double chi2ndf = fit->GetChisquare() / fit->GetNDF();
+	func->SetLineWidth(2);
+	func->SetLineColor(kRed);
+
+	double chi2ndf = func->GetChisquare() / func->GetNDF();
 
 	std::cout<<"chi2ndf "<<chi2ndf<<std::endl;
 
-	double par0 = fit->GetParameter(0);
-	double err0 = fit->GetParError(0);
-	double par1 = fit->GetParameter(1);
-	double err1 = fit->GetParError(1);
+	double par0 = func->GetParameter(0);
+	double err0 = func->GetParError(0);
+	double par1 = func->GetParameter(1);
+	double err1 = func->GetParError(1);
 
 	double xint = - par0 / par1;
-	double xint_err = RadialFieldPrecision(fit); 
+	double xint_err = BrResError( xint, par0, err0, par1, err1); 
 
 	TPaveText *names = new TPaveText(0.30,0.69,0.62,0.88,"NDC");
 	names->SetTextAlign(13);
@@ -169,6 +174,11 @@ void DrawRadialFieldLineFit(TGraphErrors *graph, std::string title, std::string 
 	values->SetTextFont(44);
 	values->SetTextSize(26);
 
+
+	//TLegend *function = new TLegend(0.11,0.79,0.45,0.89);//,"NDC");
+	//function->AddEntry(func,"B_{r} = A_{EDM} sin(#omega_{a}t) + c") ;
+	//function->SetBorderSize(0);
+
 	TLine *x_line = new TLine(gPad->GetUxmin(),0,xint,0);
 	x_line->SetLineStyle(2);
 	x_line->SetLineWidth(2);
@@ -191,7 +201,7 @@ void DrawRadialFieldLineFit(TGraphErrors *graph, std::string title, std::string 
 	names->Draw("same");
 	x_line->Draw("same");
 	y_line->Draw("same");
-	fit->Draw("same");
+	func->Draw("same");
 	
 
 	c->SaveAs((fname+".pdf").c_str());
@@ -204,114 +214,83 @@ void DrawRadialFieldLineFit(TGraphErrors *graph, std::string title, std::string 
 
 }
 
-// ==================== MAIN RESULT ====================
+// ==================== Main ====================
+int main() {
 
-double GetRadialFieldPrecision(int i_experiment, int i_subrun) { 
+	TRandom3 random;
 
-	std::vector<TGraphErrors*> QuadScans;
+	std::vector<TGraphErrors*> scans;
 	// Not nice 
 	double slopes[N_FIELD];
 	double slope_errs[N_FIELD];
-	double zeros[N_FIELD];
+	double holder[N_FIELD];
 
+	// =========== Sub-runs loop =========== 
+	for ( int i_subrun = 0; i_subrun < N_SUBRUNS; i_subrun++ ) {
 
+		std::cout<<"\nSub-runs:\t"<<SUBRUNS[i_subrun]<<std::endl;
 
 		// =========== Field setting loop =========== 
-	for ( int i_field = 0; i_field < N_FIELD; i_field++ ) {
-
+		for ( int i_field = 0; i_field < N_FIELD; i_field++ ) {
+	
 			// Printout
-		std::cout<<"\nScan number:\t"<<i_field<<std::endl;
-		std::cout<<"Br setting:\t"<<BR_APP[i_field]<<" ppm"<<std::endl; 
-		std::cout<<"True Br:\t"<<BR_APP[i_field]-BR_RES<<" ppm"<<std::endl;
-
-		double Br_true = BR_APP[i_field]-BR_RES;
-
+			std::cout<<"\nScan number:\t"<<i_field<<std::endl;
+			std::cout<<"Br setting:\t"<<BR_APP[i_field]<<" ppm"<<std::endl; 
+			std::cout<<"True Br:\t"<<BR_APP[i_field]-BR_RES<<" ppm"<<std::endl;
+	
+			double Br_true = BR_APP[i_field]-BR_RES;
+	
 			// Average vertical position
-		double y[N_QHV];
-		double ey[N_QHV];
-		double x[N_QHV];
-		double ex[N_QHV];
-
+			double y[N_QUAD];
+			double ey[N_QUAD];
+			double x[N_QUAD];
+			double ex[N_QUAD];
+	
 			// =========== Quad setting loop ==========
-		for ( int i_quad = 0; i_quad < N_QHV; i_quad++ ) {
-
+			for ( int i_quad = 0; i_quad < N_QUAD; i_quad++ ) {
+	
 				// Printout
-			std::cout<<"QHV "<<i_quad<<":\t\t"<<QHV[i_quad]<<" kV"<<std::endl;
-
-			double yPos =  yPosition( Br_true, QHV[i_quad]);
-
-				// Smear y 
-			y[i_quad] = GausSmearing(yPos, i_subrun);
-				// std::cout<<"\nyPos:\t"<<yPos<<std::endl;
-				// std::cout<<"y[i_quad]:\t"<<y[i_quad]<<std::endl;
-				// Take uncertainty as sigma
-			ey[i_quad] = SIGMA[i_subrun];
-			x[i_quad] = QHV[i_quad];
-			ex[i_quad] = 0;
-
-
-		}
-
-		TGraphErrors *QuadScan = new TGraphErrors(N_QHV,x,y,ex,ey);
-		QuadScan->SetName((std::to_string(BR_APP[i_field])+" ppm").c_str());
-
+				std::cout<<"QHV "<<i_quad<<":\t\t"<<QHV[i_quad]<<" kV"<<std::endl;
+	
+				double yPos =  yPosition( Br_true, QHV[i_quad]);
+				// Calculate y 
+				y[i_quad] = yPos;
+	
+				// Smear y_err
+				ey[i_quad] = GausSmearing(yPos, i_field, i_quad, i_subrun);
+	
+				x[i_quad] = QHV[i_quad];
+				ex[i_quad] = 0;
+	
+	
+			}
+	
+			TGraphErrors *scan = new TGraphErrors(N_QUAD,x,y,ex,ey);
+			scan->SetName((std::to_string(BR_APP[i_field])+" ppm").c_str());
+	
 			// Fit
-		TF1 *quadLineFit = new TF1("quadLineFit", "pol 1");
-		QuadScan->Fit(quadLineFit,"Q");
-		QuadScans.push_back(QuadScan);
-
-		slopes[i_field] = quadLineFit->GetParameter(1);
-		slope_errs[i_field] = quadLineFit->GetParError(1);
-		zeros[i_field] = 0;
-
-
-	} 
+			TF1 *lineFit = new TF1("lineFit", "pol 1");
+			scan->Fit(lineFit,"Q");
+			scans.push_back(scan);
+			
+			slopes[i_field] = lineFit->GetParameter(1);
+			slope_errs[i_field] = lineFit->GetParError(1);
+			holder[i_field] = 0;
 	
-	TGraphErrors *result = new TGraphErrors(N_FIELD,BR_APP,slopes,zeros,slope_errs);
-
-	TF1 *fieldLineFit = new TF1("fieldLineFit", "pol 1");
-
-		// lineFit2->SetLineColor(kRed);
-	result->Fit(fieldLineFit);
-
-		// Only draw the plots once 
-	if(i_experiment==0) { 
-		DrawQuadScanFits(QuadScans, ";QHV [kV];#LTy#GT [mm]", "../Images/MC/ToyRadialFieldScan/QuadScans_NSUBRUN_"+std::to_string(SUBRUNS[i_subrun])+"_NEXP_"+std::to_string(i_experiment),-3,2,"quadLineFit");
-		DrawRadialFieldLineFit(result,"Sub-runs "+std::to_string(SUBRUNS[i_subrun])+";Applied B_{r} [ppm];#LTy#GT / QHV [mm/kV]","../Images/MC/ToyRadialFieldScan/Result_NSUBRUN_"+std::to_string(SUBRUNS[i_subrun])+"_NEXP_"+std::to_string(i_experiment),"fieldLineFit");
-	}
-
-
-	return RadialFieldPrecision(fieldLineFit);
-
-
-}
-
-// ==================== MAIN ====================
-
-int main() {
-
-	// Loop through a range of sub-runs
-
-	for ( int i_subrun = 0; i_subrun < N_SUBRUNS; i_subrun++ ) { 
-
-		// Perform many experiments at that sub-run
-
-		// Book histogram for each sub-run 
-
-		TH1D *dBr = new TH1D("dBr","dBr",16,0,4);
-
-		for ( int i_exp = 0; i_exp < 1000; i_exp++ ) {
-
-			dBr->Fill(GetRadialFieldPrecision(i_exp, i_subrun)); 
-
-		}
-
-		DrawTH1(dBr,"Number of sub-runs: "+std::to_string(SUBRUNS[i_subrun])+";Residual #deltaB_{r} [ppm];Experiments","../Images/MC/ToyRadialFieldScan/dBr_"+std::to_string(SUBRUNS[i_subrun]));
+	
+		} 
+	
+		DrawQuadScanFits(scans, ";QHV [kV];#LTy#GT [mm]", "../Images/MC/ToyRadialFieldScan/Scans_NSUBRUN_"+std::to_string(SUBRUNS[i_subrun]),-3,2,"lineFit");
+	
+		TGraphErrors *result = new TGraphErrors(N_FIELD,BR_APP,slopes,holder,slope_errs);
+		TF1 *lineFit2 = new TF1("lineFit2", "pol 1");
+		lineFit2->SetLineColor(kRed);
+		result->Fit(lineFit2);
+		DrawRadialFieldLineFit(result,"Sub-runs "+std::to_string(SUBRUNS[i_subrun])+";Applied B_{r} [ppm];#LTy#GT / QHV [mm/kV]","../Images/MC/ToyRadialFieldScan/Result_NSUBRUN_"+std::to_string(SUBRUNS[i_subrun]));
+	
 
 	}
 
-	
 
 	return 0;
-
 }
