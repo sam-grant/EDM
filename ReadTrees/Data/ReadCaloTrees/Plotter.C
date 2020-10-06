9bc4590dc36eb5f15a6df551996dd0364683a72a
Samuel Grant <samuelgrant@x.local>
2020-10-06 11:52:26 -0500

    Cleaned up calo tree reader

--- ReadTrees/Data/ReadCaloTrees/Plotter.C
+++ ReadTrees/Data/ReadCaloTrees/Plotter.C
@@ -10,9 +10,6 @@ void Plotter::InitHistos() {
 
   for( int i = 0; i<400; i++) {
 
-    //plot1D("ctag_"+std::to_string(i),500,0,500);
-   // plot2D("clusterY_vs_clusterX_"+std::to_string(i),225,0,225,150,0,150,"Cluster position X [mm]", "Cluster position Y [mm]");
-    //plot2D("clusterY_vs_clusterX_old_"+std::to_string(i),225,0,225,150,0,150,"Cluster position X [mm]", "Cluster position Y [mm]");
     plot2D("clusterY_vs_clusterX_"+std::to_string(i),225,0,225,150,0,150,"Cluster position X [mm]", "Cluster position Y [mm]");
 
   }
@@ -21,39 +18,16 @@ void Plotter::InitHistos() {
 
 void Plotter::Run() {
 
-  //int counter = 0; 
 
-  while( NextClusterEvent() ) { // } && counter<1e6) {
+  while( NextClusterEvent() ) { 
 
-//    if(cl->clusterTime > 50000 && cl->clusterTime < 500000) {
-//      if(cl->clusterEnergy > 1800 && cl->clusterEnergy < 9300) { 
-//
-//        Fill2D("clusterY_vs_clusterX_old_"+std::to_string(cl->subRunNum),cl->clusterX*25,cl->clusterY*25); // xtal -> mm
-//
-//
-//      }
-//    }
+    // Cuts are from CTAG producer module
 
-    if(cl->clusterTime > 24000) {
-      if(cl->clusterEnergy > 1700 && cl->clusterEnergy < 6000) { 
+    if(cl->clusterEnergy > 1700 && cl->clusterEnergy < 6000 && cl->clusterTime > 24000) { 
 
         Fill2D("clusterY_vs_clusterX_"+std::to_string(cl->subRunNum),cl->clusterX*25,cl->clusterY*25); // xtal -> mm
 
-
-      }
     }
-
-//    if(cl->clusterTime < 50000 || cl->clusterTime > 500000) continue;
-//    if(cl->clusterEnergy < 1800 || cl->clusterEnergy > 9300) continue;
-//   
-//  	Fill1D("ctag_"+std::to_string(cl->subRunNum),cl->ctag);
-//    Fill2D("clusterY_vs_clusterX_"+std::to_string(cl->subRunNum),cl->clusterX*25,cl->clusterY*25); // xtal -> mm
-//
-    
-
-
-   // counter++;
-
 
   }
 
