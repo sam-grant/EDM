088719d2777426bf693b8dd765bf49725a1db003
Samuel Grant <samuelgrant@x.local>
2020-10-07 14:22:23 -0500

    Cleaned CaloMeanPos.C, removed TGraph function

--- CMacros/CaloMeanPos.C
+++ CMacros/CaloMeanPos.C
@@ -32,57 +32,30 @@ double Mean(TH2 *hist) {
 
 }
 
-// A bit superfluous but it seems to work fine
-TGraphErrors *DefineTGraph(vector<int> N, vector<double> mean, vector<double> err, int type) { 
-
-	int n = N.size();
-	double x[n];
-  	double ex[n];
-  	double y[n];
-  	double ey[n];
-
-  	for(int i = 0; i < n; i++) {
-
-  		if(type==0) {
-
-  			x[i] = N.at(i); 
-  			ex[i] = 0; 
-  			y[i] = mean.at(i); 
-      		ey[i] = err.at(i); 
-
-      	} else if(type==1) {
-
-  			x[i] = N.at(i); 
-  			ex[i] = 0; 
-  			y[i] = err.at(i); 
-      		ey[i] = 0; 
-
-      	}
-
-  	}
-
-  	return new TGraphErrors(n,x,y,ex,ey);
-}
-
-
 int main() {
+
+	int N = 349; // sub-runs in 15921
 
 	TFile *input = TFile::Open("../Plots/Data/MeanCaloPos/plots_calo_15921.root");
 
+	// Book in clusterXY hist, and their stack
 	TH2D *posHist;
 	TH2D *posHistStack;
 
-	// plot sub-runs vs ctag, write to file
-	TH2D *SUBRUN_vs_CTAG = new TH2D("SUBRUN_vs_CTAG","SUBRUN_vs_CTAG",355,0.5,355.5,14e4,0,14e6);
+	// TH1s are for reading in/out data, TGraphs are for presenting data
+	TH1D *CTAG_vs_SUBRUN = new TH1D("CTAG_vs_SUBRUN","CTAG_vs_SUBRUN",N,0.5,349.5); 
+	TH1D *SIGMA_vs_SUBRUN = new TH1D("SIGMA_vs_SUBRUN","SIGMA_vs_SUBRUN",N,0.5,349.5);
 
-	vector<int> ctag_; 
-	vector<double> meanY_; 
-	vector<double> meanYErr_;
+	// Input for TGraphs 
+	double x[N];
+  	double y[N]; 
+  	double ey[N];
+  	double zeros[N];
 
+  	// Counter
 	int subruns = 1;
 
 	// Loop through sub-runs 
-
 	// Note that i =/= n_subruns
 	for(int i = 0; i<400; i++) { 
 
@@ -104,70 +77,49 @@ int main() {
 		double meanY = posHistStack->ProjectionY()->GetMean();
 		double meanYErr = posHistStack->ProjectionY()->GetMeanError();
 
-		// Append them to a vector
-		ctag_.push_back(ctag);
-		meanY_.push_back(meanY);
-		meanYErr_.push_back(meanYErr);	
+		// Append them into arrays
+		x[subruns] = ctag;
+		y[subruns] = meanY; 
+		ey[subruns] = meanYErr; 
+		zeros[subruns] = 0;
 
- 		// Fill sub-run vs CTAG
- 		SUBRUN_vs_CTAG->Fill(subruns, ctag);
+ 		CTAG_vs_SUBRUN->SetBinContent(subruns, ctag);
+ 		SIGMA_vs_SUBRUN->SetBinContent(subruns, meanYErr);
 
 		subruns++;
 
 	}
 
 	// Draw two axis TGraphs for y-pos versus positrons
+	TGraphErrors *meanY_vs_subruns = new TGraphErrors(N,x,y,zeros,ey); // DefineTGraph(ctag_, meanY_, meanYErr_, 0);
+	TGraphErrors *meanYErr_vs_subruns = new TGraphErrors(N,x,ey,zeros,zeros);
 
-	TGraphErrors *meanY_vs_subruns = DefineTGraph(ctag_, meanY_, meanYErr_, 0);
-	TGraphErrors *meanYErr_vs_subruns = DefineTGraph(ctag_, meanY_, meanYErr_, 1);
-
-	meanY_vs_subruns->GetXaxis()->SetRangeUser(0,ctag_.back());
-	meanYErr_vs_subruns->GetXaxis()->SetRangeUser(0,ctag_.back());
+	meanY_vs_subruns->GetXaxis()->SetRangeUser(0,x[subruns-1]);
+	meanYErr_vs_subruns->GetXaxis()->SetRangeUser(0,x[subruns-1]);
 	meanY_vs_subruns->GetYaxis()->SetRangeUser(75.45,76.1);
 	meanYErr_vs_subruns->GetYaxis()->SetRangeUser(0,0.25);
 
-	DrawTGraphErrorsDoubleXAxis(meanY_vs_subruns, ";CTAGs;Cluster mean Y-position [mm]", "Number of sub-runs", "../Images/Data/CaloMeanPos/CaloMeanYPos_vs_CTAG", 1, subruns);
-	DrawTGraphErrorsDoubleXAxis(meanYErr_vs_subruns, ";CTAGs;Cluster mean Y-position uncertainty [mm]", "Number of sub-runs", "../Images/Data/CaloMeanPos/CaloMeanYPosErr_vs_CTAG", 1, subruns);
+	DrawTGraphErrorsDoubleXAxis(meanY_vs_subruns, ";CTAGs;Cluster mean Y-position [mm]", "Number of sub-runs", "../Images/Data/CaloMeanPos/CaloMeanYPos_vs_CTAG", 0, subruns);
+	DrawTGraphErrorsDoubleXAxis(meanYErr_vs_subruns, ";CTAGs;Cluster mean Y-position uncertainty [mm]", "Number of sub-runs", "../Images/Data/CaloMeanPos/CaloMeanYPosErr_vs_CTAG", 0, subruns);
 
-	// Draw TH2 of CTAG vs subrun and write to file
-	TFile *output = new TFile("../Plots/Data/MeanCaloPos/SUBRUN_vs_CTAG_15921.root", "RECREATE");
-	DrawTH2(SUBRUN_vs_CTAG,";Sub-runs;CTAGs","../Images/Data/CaloMeanPos/SUBRUN_vs_CTAG_15921");
-	SUBRUN_vs_CTAG->SetDirectory(gDirectory);
+	// Write everything to ROOT file
+	string outName = "../Plots/Data/MeanCaloPos/CTAG_vs_SUBRUN_15921.root";
+	TFile *output = new TFile(outName.c_str(), "RECREATE");
+
+	// Write TGraphs to ROOT file
+	// meanY_vs_subruns->SetName("meanY_vs_subruns");
+	// meanY_vs_subruns->Write();
+	// meanYErr_vs_subruns->SetName("meanYErr_vs_subrun");
+	// meanYErr_vs_subruns->Write();
+
+	// Draw TH1 of CTAG vs subrun and write to ROOT file
+	DrawTH1(CTAG_vs_SUBRUN,";Sub-runs;CTAGs","../Images/Data/CaloMeanPos/CTAG_vs_SUBRUN_15921");
+	DrawTH1(SIGMA_vs_SUBRUN,";Sub-runs;#sigma_{y}","../Images/Data/CaloMeanPos/SIGMA_vs_SUBRUN_15921");
+	CTAG_vs_SUBRUN->Write();
+	SIGMA_vs_SUBRUN->Write();
 	output->Write();
+
+	cout<<"Written:\t"<<outName<<endl;
 
 	return 0;
 }
-
-/*		if(subruns==25) {
-			cout<<"SIGMA at 25 sub-runs:\t"<<meanYErr<<endl;
-			cout<<"CTAGs at 25 sub-runs:\t"<<ctag<<endl;
-		} else if(subruns==50) {
-			cout<<"SIGMA at 50 sub-runs:\t"<<meanYErr<<endl;
-			cout<<"CTAGs at 50 sub-runs:\t"<<ctag<<endl;
-		} else if(subruns==75) {
-			cout<<"SIGMA at 75 sub-runs:\t"<<meanYErr<<endl;
-			cout<<"CTAGs at 75 sub-runs:\t"<<ctag<<endl;
-		} else if(subruns==100) {
-			cout<<"SIGMA at 100 sub-runs:\t"<<meanYErr<<endl;
-			cout<<"CTAGs at 100 sub-runs:\t"<<ctag<<endl;
-		} else if(subruns==125) {
-			cout<<"SIGMA at 125 sub-runs:\t"<<meanYErr<<endl;
-			cout<<"CTAGs at 125 sub-runs:\t"<<ctag<<endl;
-		} else if(subruns==150) {
-			cout<<"SIGMA at 150 sub-runs:\t"<<meanYErr<<endl;
-			cout<<"CTAGs at 150 sub-runs:\t"<<ctag<<endl;
-		} else if(subruns==175) {
-			cout<<"SIGMA at 175 sub-runs:\t"<<meanYErr<<endl;
-			cout<<"CTAGs at 175 sub-runs:\t"<<ctag<<endl;
-		} else if(subruns==200) {
-			cout<<"SIGMA at 200 sub-runs:\t"<<meanYErr<<endl;
-			cout<<"CTAGs at 200 sub-runs:\t"<<ctag<<endl;
-		}
- 
- 		if(ctag > 7.99e6 && ctag < 8.01e6) {
- 			cout<<"Sub-runs at "<<ctag<<" CTAGS: "<<subruns<<endl;
- 		} 
-
- 		if(ctag > 4.8e5 && ctag < 5.2e5) {
- 			cout<<"Sub-runs at "<<ctag<<" CTAGS: "<<subruns<<endl;
- 		}*/ 
