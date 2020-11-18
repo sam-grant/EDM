177174165e94c7042b51381a6215104464047d27
Samuel Grant <samuelgrant@x.local>
2020-11-18 16:12:42 -0600

    Radial field scan in beam gun sim

--- CMacros/BeamGunTuningPlotter.C
+++ CMacros/BeamGunTuningPlotter.C
@@ -7,39 +7,38 @@
 
 using namespace std;
 
-int main() {
+void GetTuningPlots(vector<string> names_) {
 
-	TFile *input = TFile::Open("../Plots/MC/RadialField/BeamGunTuning/beamGunTuningPlots.root");
 
-	TH1D *h_y = (TH1D*)input->Get("all/Y");
-	TH1D *h_p = (TH1D*)input->Get("all/P");
+	for( int i_file = 0; i_file < names_.size(); i_file++ ) {
 
-	TH2D *h_y_vs_t = (TH2D*)input->Get("all/Y_vs_t");
-	TH2D *h_px_vs_t = (TH2D*)input->Get("all/Px_vs_t");
-	TH2D *h_pz_vs_t = (TH2D*)input->Get("all/Pz_vs_t");
+		TFile *input = TFile::Open( ("../Plots/MC/RadialField/BeamGunTuning/beamGunTuningPlots_"+names_.at(i_file)+".root").c_str() );
 
-	DrawTH1(h_y,";Y [mm];Ghost tracks","../Images/MC/BeamGunTuning/Y");
-	DrawTH1(h_p,";Momentum [MeV];Ghost tracks","../Images/MC/BeamGunTuning/P");
+		TH1D *h_y = (TH1D*)input->Get("all/Y");
+		TH1D *h_p = (TH1D*)input->Get("all/P");
 
-	DrawTH2(h_y_vs_t,";t [#mus (maybe)];Y [mm]","../Images/MC/BeamGunTuning/Y_vs_t");
-	DrawTH2(h_px_vs_t,";t [#mus (maybe)];Px [MeV]","../Images/MC/BeamGunTuning/Px_vs_t");
-	//h_pz_vs_t->GetYaxis()->SetRangeUser(0,40);
-	DrawTH2(h_pz_vs_t,";t [#mus (maybe)];Pz [MeV]","../Images/MC/BeamGunTuning/Pz_vs_t");
+		TH2D *h_y_vs_t = (TH2D*)input->Get("all/Y_vs_t");
+		TH2D *h_px_vs_t = (TH2D*)input->Get("all/Px_vs_t");
+		TH2D *h_pz_vs_t = (TH2D*)input->Get("all/Pz_vs_t");
 
-	// Go plane by plane
-	int nPlanes = 48;
+		DrawTH1(h_y,names_.at(i_file)+";Y [mm];Ghost tracks", ("../Images/MC/BeamGunTuning/"+names_.at(i_file)+"/Y").c_str() );
+		DrawTH1(h_p,names_.at(i_file)+";Momentum [MeV];Ghost tracks", ("../Images/MC/BeamGunTuning/"+names_.at(i_file)+"/P").c_str() );
 
-/*	for( int i_plane = 0; i_plane < nPlanes; i_plane++ ) {
-
-		h_y = (TH1D*)input->Get( ("all/planes/Y_"+to_string(i_plane)).c_str() );
-		h_p = (TH1D*)input->Get( ("all/planes/P_"+to_string(i_plane)).c_str() );
-
-		DrawTH1(h_y,"Plane "+to_string(i_plane)+";Y [mm];Ghost tracks", "../Images/MC/BeamGunTuning/Planes/Y_"+to_string(i_plane) );
-		DrawTH1(h_p,"Plane "+to_string(i_plane)+";Momentum [MeV];Ghost tracks", "../Images/MC/BeamGunTuning/Planes/P_"+to_string(i_plane) );
+		DrawTH2(h_y_vs_t,names_.at(i_file)+";t [ns];Y [mm]", ("../Images/MC/BeamGunTuning/"+names_.at(i_file)+"/Y_vs_t").c_str() );
+		DrawTH2(h_px_vs_t,names_.at(i_file)+";t [ns];Px [MeV]", ("../Images/MC/BeamGunTuning/"+names_.at(i_file)+"/Px_vs_t").c_str() );
+		DrawTH2(h_pz_vs_t,names_.at(i_file)+";t [ns];Pz [MeV]", ("../Images/MC/BeamGunTuning/"+names_.at(i_file)+"/Pz_vs_t").c_str() );
 
 	}
 
-*/
+	return;
+
+}
+int main() {
+
+	vector<string> names_; 
+	names_.push_back("-50ppm"); names_.push_back("0ppm"); names_.push_back("50ppm");
+
+	GetTuningPlots(names_);
 
 	return 0;
 }
